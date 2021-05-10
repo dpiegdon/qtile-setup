ec0ec26fc1d049ef48c6857ee564ec569d4fb878
David R. Piegdon <dgit@piegdon.de>
2021-05-10 14:40:52 +0000

    clock stuff

--- config/config_widgets.py
+++ config/config_widgets.py
@@ -63,8 +63,9 @@ def init_widgets(theme, fontsize, iconsize):
             widget.GenPollText(func=config_funcs.get_net_diag, update_interval=30, foreground='#880088'),
             widget.GenPollText(func=config_funcs.get_dirty_mem_M, update_interval=15, foreground='#ff4400'),
             widget.Clock(format='%a %Y-%m-%d'),
-            widget.Clock(format='%H:%M %Z', foreground='#ff9999'),
-            widget.Clock(format='%H:%M %Z', timezone="EST5EDT", foreground='#444477'),
+            widget.Clock(format='%H:%M %Z', foreground='#ff6666'),
+            widget.Clock(format='%H:%M %Z', timezone="EST5EDT", foreground='#6666ff'),
+            widget.Clock(format='%H:%M %Z', timezone="Europe/Berlin", foreground='#ffc000'),
             widget.Systray(icon_size=iconsize),
         ]
     return widgets
